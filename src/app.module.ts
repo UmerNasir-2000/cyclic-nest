e65a45aae834b8619fb47aefba8913febdbafd04
Umer Nasir <unasir@dawaai.pk>
2022-11-11 10:05:12 +0500

    Initial commit...

---
+++ src/app.module.ts
@@ -0,0 +1,15 @@
+import { Module, OnModuleInit } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+
+@Module({
+  imports: [ConfigModule.forRoot()],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule implements OnModuleInit {
+  onModuleInit() {
+    console.log('Application started...');
+  }
+}
